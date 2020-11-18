3ab25a71a36d9df31292b506fd5aed2aca949b7f
javieter <olmedo_javier@hotmail.com>
2020-11-18 20:42:10 -0300

    agregado de latest por id

--- routes/historicoQR.js
+++ routes/historicoQR.js
@@ -11,6 +11,12 @@ router.get('/latest', async (req, res) => {
   res.send(await historical.GetLatest());
 });
 
+router.get('/usuario/:id/latest', async (req, res) => {
+  const result = await attendances.GetLatestByUserId(req.params.id);
+  if (result !== null) res.json(result);
+  else res.sendStatus(403);
+});
+
 router.get('/:id', async (req, res) => {
   const result = await historical.GetById(req.params.id);
   if (result !== null) res.json(result);
