ec1a43623a27ebf0ce227b9b83d704abcc0e558c
javieter <olmedo_javier@hotmail.com>
2020-11-18 21:03:26 -0300

    cambio en asistencias.js

--- routes/historicoQR.js
+++ routes/historicoQR.js
@@ -11,12 +11,6 @@ router.get('/latest', async (req, res) => {
   res.send(await historical.GetLatest());
 });
 
-router.get('/usuario/:id/latest', async (req, res) => {
-  const result = await attendances.GetLatestByUserId(req.params.id);
-  if (result !== null) res.json(result);
-  else res.sendStatus(403);
-});
-
 router.get('/:id', async (req, res) => {
   const result = await historical.GetById(req.params.id);
   if (result !== null) res.json(result);
